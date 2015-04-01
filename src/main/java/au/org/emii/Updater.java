69540b70a814947c5127cd1f4c013884f59e93e0
julian1 <julian.atkinson71@gmail.com>
2015-04-01 14:43:17 +1100

    update

--- src/main/java/au/org/emii/Updater.java
+++ src/main/java/au/org/emii/Updater.java
@@ -1,148 +1,139 @@
 
-// rm *.class ; javac main.java  ; java main
-
-// time javac test2.java -cp .:netcdfAll-4.2.jar
-// time java -cp .:postgresql-9.1-901.jdbc4.jar:netcdfAll-4.2.jar  test2
-
 
 package au.org.emii;
 
-//import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.InputStream ;
-import java.io.IOException;
-
 import java.io.FileInputStream;
-import java.io.BufferedInputStream;
-
-import java.util.ArrayList; //io.BufferedInputStream;
-import java.util.List; //io.BufferedInputStream;
-//import java.util.ArrayDouble; //io.BufferedInputStream;
-
-import java.util.HashMap; //io.BufferedInputStream;
-import java.util.Map; //io.BufferedInputStream;
-
-import java.util.Date; 
+import java.io.StringReader;
+import java.io.StringWriter;
 
 import java.sql.*;
 
 import java.util.Properties;
-import java.lang.RuntimeException;
 
-import java.text.SimpleDateFormat;
-/*import java.util.List;
-import java.util.Set;
-import java.util.ArrayList;
-*/
-//import java.util.Date;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
-import ucar.nc2.NetcdfFileWriteable;
-import ucar.nc2.Dimension;
-//import ucar.nc2.DataType.DOUBLE;
-//import ucar.nc2.*;
-
-import ucar.ma2.DataType;
-import ucar.ma2.Array;
-
-import ucar.ma2.ArrayDouble;
-import ucar.ma2.ArrayFloat;
-import ucar.ma2.ArrayByte;
-//import ucar.ma2.ArrayString;
-
-import ucar.ma2.Index;
-
-import java.util.Arrays;
-
-import java.util.regex.Pattern ;
-import java.util.regex.Matcher;
-
-
-import java.util.Iterator;
-
-/*
-import au.org.emii.geoserver.extensions.filters.layer.data.FilterConfiguration;
-import org.apache.commons.io.IOUtils;
-import org.junit.Test;
-import org.xml.sax.SAXException;
-*/
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.ByteArrayInputStream;
-// import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-
-import java.util.AbstractMap.SimpleImmutableEntry;
-// java.util.AbstractMap.SimpleImmutableEntry<K,V>;
+import org.apache.commons.cli.*;
 
 
 
-//import java.util.StringTokenizer;
-
-// string tokenizer isn't going to work because may not be strings.
-
-// do everything twice?
-// or leave the value there...
+// xslt examples,
+// http://fandry.blogspot.com.au/2012/04/java-xslt-processing-with-saxon.html
 
 
-// two interfaces a builder to generate, and then a class to use.
+
 
 class Updater 
 {
 
 	public Updater ()
-	{
+	{ }
 
+	private static Transformer getTransformer( String filename ) throws Exception 
+	{
+		final TransformerFactory tsf = TransformerFactory.newInstance();
+		final InputStream is  = new FileInputStream( "trans.xslt" ); 
+
+		return tsf.newTransformer(new StreamSource(is));
 	}
 
-	// not sure if we should do this here...
-
-	// certainly configuration should be pulled out... and put in resources.
-
-    public static Connection getConn() throws Exception
+	private static String transform ( Transformer transformer, String xmlString) throws Exception 
 	{
-		//String url = "jdbc:postgresql://127.0.0.1/postgres";
+		final StringReader xmlReader = new StringReader(xmlString);
+		final StringWriter xmlWriter = new StringWriter();
 
-		// psql -h test-geoserver -U meteo -d harvest
+		transformer.transform(new StreamSource(xmlReader), new StreamResult(xmlWriter));
 
-		String url = "jdbc:postgresql://test-geoserver/harvest";
+		return xmlWriter.toString();
+	}
+
+    private static Connection getConn( String url, String user, String pass) throws Exception
+	{
 		Properties props = new Properties();
-		props.setProperty("user","meteo");
-		props.setProperty("password","meteo");
 
-//		props.setProperty("search_path","soop_sst,public");
-/*
-		String url = "jdbc:postgresql://dbprod.emii.org.au/harvest";
-		Properties props = new Properties();
-		props.setProperty("user","jfca");
-		props.setProperty("password","fredfred");
-*/
+		props.setProperty("user", user );
+		props.setProperty("password",pass );
 
+		// props.setProperty("search_path","soop_sst,public");
 		props.setProperty("ssl","true");
 		props.setProperty("sslfactory","org.postgresql.ssl.NonValidatingFactory");
 		props.setProperty("driver","org.postgresql.Driver" );
 
-		Connection conn = DriverManager.getConnection(url, props);
-		/*if(conn == null) {
-			throw new RuntimeException( "Could not get connection" );
-		}*/
-		return conn;
+		return DriverManager.getConnection(url, props);
 	}
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception
 	{
-		System.out.println( "hello world" ); 
+		Options options = new Options();
+		options.addOption("url", true, "jdbc connection string, eg. jdbc:postgresql://127.0.0.1/geonetwork");
+		options.addOption("u", true, "user");
+		options.addOption("p", true, "password");
+		options.addOption("uuid", true, "metadata record uuid");
+		options.addOption("help", false, "show help");
+		options.addOption("t", true, "xslt file for transform");
+		options.addOption("stdout", false, "dump result to stdout");
+		options.addOption("update", false, "actually update the record");
+
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse( options, args);
+
+		if(cmd.hasOption("help") || !cmd.hasOption("url") || !cmd.hasOption("u") || !cmd.hasOption("u")) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp( "Updater", options );
+			return;
+		}
+
+		Connection conn = getConn( 
+			cmd.getOptionValue("url"), 
+			cmd.getOptionValue("u"), 
+			cmd.getOptionValue("p")
+		); 
+
+		String query = "SELECT id,uuid,data FROM metadata "  ;
+		if( cmd.hasOption("uuid")) {
+			query += " where uuid = '" + cmd.getOptionValue("uuid") + "'"; 
+		} 
+
+        PreparedStatement stmt = conn.prepareStatement( query );
+        ResultSet rs = stmt.executeQuery();
+
+		Transformer transformer = null; 
+		if( cmd.hasOption("t")) { 
+			transformer = getTransformer(cmd.getOptionValue("t") );
+		}	 
+
+        int count = 0;
+        while ( rs.next() ) 
+        {
+			int id = (Integer) rs.getObject("id");
+			String uuid = (String) rs.getObject("uuid");
+			String data = (String) rs.getObject("data");
+
+			if( transformer != null ) {
+				data = transform( transformer, data);  
+			}
+	
+			if( cmd.hasOption("stdout")) {
+				System.out.println( "id " + id );
+				System.out.println( "uuid " + uuid );
+				System.out.println(data);
+			}
+
+			if( cmd.hasOption("update")) {
+				PreparedStatement updateStmt = conn.prepareStatement( "update metadata set data=? where id=?" ) ; 
+				updateStmt.setString(1, data );
+				updateStmt.setInt(2, id );
+				updateStmt.executeUpdate();
+			}
+
+            ++count;
+			break;
+        }
+
+        System.out.println( "records processed " + count );
 	}
 }
 
