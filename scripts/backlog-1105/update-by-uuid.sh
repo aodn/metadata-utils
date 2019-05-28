d67f621af3e75ede7e8c9ecb682141e8b87a1ee9
Craig Rose <cm.rose@utas.edu.au>
2019-05-29 09:50:11 +1000

    database host and user as parameters

--- scripts/backlog-1105/update-by-uuid.sh
+++ scripts/backlog-1105/update-by-uuid.sh
@@ -3,6 +3,9 @@
 # We are given a xml file and extract the geonetwork uuids from it to find records for updating
 
 # $1 : show, trial, update
+# $2 : database host
+# $3 : database user
+# $4 : database password
 
 for uuid in $(grep "<identifier type=\"global\">" geoserver.imos.org.au_geoserver_wms.xml | cut -c39-74)
   do
@@ -10,16 +13,16 @@ for uuid in $(grep "<identifier type=\"global\">" geoserver.imos.org.au_geoserve
       show)
         # Display the records to be changed
         java -jar ../../mafia/target/mafia-1.0.0.jar \
-            -url jdbc:postgresql://172.28.128.3/geonetwork_aodn \
-            -user geonetwork_aodn -pass fzYp6XMZ0fAk2wp9CMKmUqGF \
+            -url jdbc:postgresql://"$2"/geonetwork_aodn \
+            -user "$3" -pass "$4" \
             -uuid $uuid \
             -stdout
         ;;
       trial)
         # Display changes to records without updating
         java -jar ../../mafia/target/mafia-1.0.0.jar \
-            -url jdbc:postgresql://172.28.128.3/geonetwork_aodn \
-            -user geonetwork_aodn -pass fzYp6XMZ0fAk2wp9CMKmUqGF \
+            -url jdbc:postgresql://"$2"/geonetwork_aodn \
+            -user "$3" -pass "$4" \
             -uuid $uuid \
     	    -transform backlog-1105.xslt \
             -stdout
@@ -27,8 +30,8 @@ for uuid in $(grep "<identifier type=\"global\">" geoserver.imos.org.au_geoserve
       update)
         # Update
         java -jar ../../mafia/target/mafia-1.0.0.jar \
-            -url jdbc:postgresql://172.28.128.3/geonetwork_aodn \
-            -user geonetwork_aodn -pass fzYp6XMZ0fAk2wp9CMKmUqGF \
+            -url jdbc:postgresql://"$2"/geonetwork_aodn \
+            -user "$3" -pass "$4" \
             -uuid $uuid \
             -transform backlog-1105.xslt \
             -update
